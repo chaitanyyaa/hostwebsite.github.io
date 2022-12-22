a6ec9ca567fae404d6102392cb972ac46f6fa614
chaitanyyaa <105532837+chaitanyyaa@users.noreply.github.com>
2022-12-22 12:32:35 +0530

    Last commit

---
+++ script.js
@@ -0,0 +1,26 @@
+jQuery('#frm').validate({
+	rules:{
+		name:"required",
+		email:{
+			required:true,
+			email:true
+		},
+		password:{
+			required:true,
+			minlength:5
+		},
+	},messages:{
+		name:"Please enter your name",
+		email:{
+			required:"Please enter email",
+			email:"Please enter valid email",
+		},
+		password:{
+			required:"Please enter your password",
+			minlength:"Password must be 5 char long"
+		},
+	},
+	submitHandler:function(form){
+		form.submit();
+	}
+});
